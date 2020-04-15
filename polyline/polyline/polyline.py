908f2fc1e3f95e2a0ff03bd71bc8c41fd8215a1c
Thorben Menne <mennthor@aol.com>
2020-04-15 18:39:46 +0200

    Added gradient of distance to line

--- polyline/polyline/polyline.py
+++ polyline/polyline/polyline.py
@@ -4,6 +4,7 @@ import numpy as np
 class Polyline(object):
 
     def __init__(self, vertices=None):
+        """ Vertices must have shape (nvertices, 2). """
         self._vertices = np.empty((0, 2))
         self._segment_lengths = np.empty(0)
         if vertices is not None:
@@ -39,37 +40,52 @@ class Polyline(object):
         v = (v1 - v0) * t_norm.reshape(len(t), 1) + v0
         return v
 
-    def get_dist_to_line(self, pt):
+    def get_dist_to_line(self, pts):
         """
         Returns the smallest distance to any line segment in the polyline.
+        Points must have shape (npoints, 2).
 
         Returns projection vectors from point to clostest point on the line, the
         distances and an array which encodes to which entity the distance was
         clostest to (integer >= 0: vertex id,
         integer < 0: -1 * (segment id + 1)).
+
+        Gradients are not continuous at projected segment borders and thus only
+        valid if the corresponding point does not project onto another one.
+
+        Returns: dists, dists_grad, proj_to, proj_vecs
         """
+        pts = np.atleast_2d(pts)
+        if len(pts) == 0 or len(pts.shape) != 2 or pts.shape[1] != 2:
+            raise ValueError("pts must have shape (npts, 2).")
+
         # Call dist to segment for each segment, then filter results
-        proj_vecs, dists, proj_to = [], [], []
+        dists, dists_grad, proj_to, proj_vecs = [], [], [], []
         for seg_idx in range(self.nsegments):
-            _pv, _d, _pt = self._get_dist_to_segment(pt, seg_idx)
-            proj_vecs.append(_pv)
+            _d, _dg, _pt, _pv = self._get_dist_to_segment(pts, seg_idx)
             dists.append(_d)
-            # Shift IDs to global indices
+            dists_grad.append(_dg)
+            proj_vecs.append(_pv)
+            # Shift local segment projection IDs to global indices
             m = _pt < 0
             _pt[m] = _pt[m] * (seg_idx + 1)  # segments: -1, -2, ..., -nsegs
             _pt[~m] = _pt[~m] + seg_idx  # Vertices: 0, 1, ..., nverts - 1
             proj_to.append(_pt)
 
-        # Argmin for all segments is closest to whole curve
+        # Argmin over all segments is closest distance from point to whole curve
         idx = np.argmin(dists, axis=0)
-        proj_vecs = np.swapaxes(proj_vecs, 0, 1)
+        # Bring in correct shape to broadcast argmin indices for all points
         dists = np.asarray(dists).T
+        dists_grad = np.swapaxes(dists_grad, 0, 1)
         proj_to = np.asarray(proj_to).T
-        proj_vecs = np.asarray([pvi[i] for pvi, i in zip(proj_vecs, idx)])
+        proj_vecs = np.swapaxes(proj_vecs, 0, 1)
+        # Put proper distances, etc. in output
         dists = np.asarray([di[i] for di, i in zip(dists, idx)])
+        dists_grad = np.asarray([dgi[i] for dgi, i in zip(dists_grad, idx)])
         proj_to = np.asarray([pti[i] for pti, i in zip(proj_to, idx)])
+        proj_vecs = np.asarray([pvi[i] for pvi, i in zip(proj_vecs, idx)])
 
-        return proj_vecs, dists, proj_to
+        return dists, dists_grad, proj_to, proj_vecs
 
     def get_cos_angle_at_vertex(self, idx):
         """
@@ -77,17 +93,10 @@ class Polyline(object):
         idx must be in [1, nvertices - 1] and the gradient in (x, y) direction
         for the given vertex coordinates.
         """
-        try:
-            idx + 1
-        except Exception:
-            raise ValueError(
-                "Only a single vertex can be removed, not a range.")
-        if isinstance(idx, np.ndarray):
-            raise ValueError(
-                "Only a single vertex can be removed, not a range.")
+        self._check_idx(idx)
 
         if idx < 1 or idx > self.nsegments:
-            raise ValueError("idx must be in (1, nvertices)")
+            raise ValueError("idx must be in [1, nsegments]")
 
         v0 = -self._vertices[idx - 1] + self._vertices[idx]  # Segment (i-1, i)
         v1 = -self._vertices[idx] + self._vertices[idx + 1]  # Segment (i, i+1)
@@ -128,22 +137,16 @@ class Polyline(object):
         Insert vertices v to the polyline at position idx, before the existing
         element. If idx == nvertices, v is appended
         """
-        try:
-            idx + 1
-        except Exception:
-            raise ValueError(
-                "Only a single vertex can be removed, not a range.")
-        if isinstance(idx, np.ndarray):
-            raise ValueError(
-                "Only a single vertex can be removed, not a range.")
+        self._check_idx(idx)
 
         nvertices_before = self.nvertices
-        if idx > nvertices_before or idx < 0:
-            raise ValueError("index out of bounds for current polyline.")
-        if len(v) == 0:
-            raise ValueError("Invalid vertex given")
+        if idx < 0 or idx > nvertices_before:
+            raise ValueError("Index out of bounds for current polyline.")
 
         v = np.atleast_2d(v)
+        if len(v) == 0 or len(v.shape) != 2 or v.shape[1] != 2:
+            raise ValueError("Single vertex must have shape (2,) or (1, 2)")
+
         ninserted = v.shape[0]
         self._vertices = np.insert(self._vertices, idx, v, axis=0)
         if self.nvertices > 1:
@@ -166,23 +169,17 @@ class Polyline(object):
     def insert_vertex_between(self, idx, scale):
         """
         Inserts a new vertex on the segment before vertex at idx, so between
-        vertex idx - 1 and vertex idx (>0) if existing (polyline must have at
+        vertex idx-1 and vertex idx (>0) if existing (polyline must have at
         least idx vertices).
         The distance to v0 and v1 is defined by scale, where scale = 0 means the
         new vertex is the same as v0, scale = 0.5 is exactly the middle between
         v0 and v1 and scale = 1 means the new vertex is the same as v1.
         """
-        try:
-            idx + 1
-        except Exception:
-            raise ValueError(
-                "Only a single vertex can be removed, not a range.")
-        if isinstance(idx, np.ndarray):
-            raise ValueError(
-                "Only a single vertex can be removed, not a range.")
+        self._check_idx(idx)
 
         if idx < 1 or idx > self.nsegments:
-            raise ValueError("idx must be in (1, nvertices)")
+            raise ValueError("idx must be in [1, nsegments]")
+
         if scale < 0. or scale > 1.:
             raise ValueError("scale must be in [0, 1]")
 
@@ -199,14 +196,10 @@ class Polyline(object):
 
     def remove_vertex(self, idx):
         """ Removes vertex at idx (in [0, nvertices-1]) """
-        try:
-            idx + 1
-        except Exception:
-            raise ValueError(
-                "Only a single vertex can be removed, not a range.")
-        if isinstance(idx, np.ndarray):
-            raise ValueError(
-                "Only a single vertex can be removed, not a range.")
+        self._check_idx(idx)
+
+        if idx < 0 or idx > self.nvertices - 1:
+            raise ValueError("idx must be in [0, nvertices - 1]")
 
         nvertices_before = self.nvertices
         self._vertices = np.delete(self._vertices, idx, axis=0)
@@ -227,21 +220,16 @@ class Polyline(object):
 
     def replace_vertex(self, idx, v):
         """ Replace vertex at idx with v """
-        try:
-            idx + 1
-        except Exception:
-            raise ValueError(
-                "Only a single vertex can be removed, not a range.")
-        if isinstance(idx, np.ndarray):
-            raise ValueError(
-                "Only a single vertex can be removed, not a range.")
-        if idx >= self.nvertices or idx < 0:
-            raise ValueError("index out of bounds for current polyline.")
+        self._check_idx(idx)
 
-        if len(v) == 0:
-            raise ValueError("Invalid vertex given")
+        if idx < 0 or idx > self.nvertices - 1:
+            raise ValueError("Index out of bounds for current polyline.")
 
-        self._vertices[[idx]] = np.atleast_2d(v)
+        v = np.atleast_2d(v)
+        if len(v) == 0 or len(v.shape) != 2 or v.shape[1] != 2:
+            raise ValueError("Single vertex must have shape (2,) or (1, 2)")
+
+        self._vertices[[idx]] = v
         if self.nsegments > 0:
             if idx == 0:
                 slen = self._get_segment_lengths(self._vertices[:2])
@@ -265,15 +253,17 @@ class Polyline(object):
     def get_segment_length(self, idx):
         return self._segment_lengths[idx]
 
-    def get_segment_length_grad(self, idx, side="last"):
+    def get_segment_length_grad(self, idx, which="last"):
         """
         Returns the gradient [dL/dx, dL/dy] for the segment idx from vertex
         v_idx to v_(idx+1) with respect to moving one of the vertices.
-        If which is 'first', then the gradient for the first vertex is
+        If 'which' is 'first', then the gradient for the first vertex is
         calcualated, if 'last', then the second vertex is used for the gradient.
         """
-        if side not in ["first", "last"]:
-            raise ValueError("'side' must be one of 'first', 'last'.")
+        self._check_idx(idx)
+
+        if which not in ["first", "last"]:
+            raise ValueError("'which' must be one of 'first', 'last'.")
 
         # L = sqrt((v1x - v0x)**2 + (v1y - v0y)**2)  for segment (v0->v1)
         # dL/dx = +(v1x - v0x) / L  for x with respect to v1
@@ -281,9 +271,8 @@ class Polyline(object):
         L = self.get_segment_length(idx)
         v0, v1 = self._vertices[idx:idx + 2]
         dv = v1 - v0
-        if side == "first":
+        if which == "first":
             dv = -dv  # Gradient changes sign if first vertex is moved
-
         return dv / L
 
     def clear(self):
@@ -302,40 +291,50 @@ class Polyline(object):
         return np.asarray(np.sqrt(
             (v[:-1, 0] - v[1:, 0])**2 + (v[:-1, 1] - v[1:, 1])**2))
 
-    def _get_dist_to_segment(self, pt, idx):
+    def _get_dist_to_segment(self, pts, idx):
         """
-        Get distance of pt to specified segment idx in the polyline.
+        Get distance of pts to specified segment idx in the polyline.
+        idx is integer, pts has shape (npts, 2).
 
         Returns projection vectors from point to clostest point on segment, the
         distances and an array which encodes to which entity the distance was
         clostest to (0: first vertex, -1: segment line, +1: second vertex).
+
+        The returned gradients are only valid for the current projection entity:
+        If projected on the line segment, the gradient is only valid for the
+        assumption, that the segment stays the closest porjection entity. The
+        same is valid for the vertices.
+        The gradients are therefor not continuos for projections at the line
+        segment boundaries.
+
+        Returns: dists, dists_grad, proj_to, proj_vecs
         """
-        pt = np.atleast_2d(pt)
-        v0 = self._vertices[[idx]]
-        v1 = self._vertices[[idx + 1]]
-        # dproj = projection vector, dpt = pt vector relative to v0
-        dproj = v1 - v0
-        dpt = pt - v0
+        v0 = self._vertices[[idx]]  # constant, shape (1, 2)
+        v1 = self._vertices[[idx + 1]]  # constant, shape (1, 2)
+        # dproj = projection vector, dpt = pts vector relative to v0
+        dproj = v1 - v0  # constant, shape (1, 2)
+        dpt = pts - v0  # shape (npts, 2)
         norm_dproj = np.linalg.norm(dproj)
         norm_dpt = np.linalg.norm(dpt, axis=1)
 
         if np.isclose(norm_dproj, 0):
-            # Both vertices are almost equal, return distance pt to v0
-            return dpt, norm_dpt
+            # Both vertices are almost equal, return distance pts to v0
+            return (norm_dpt, dpt / norm_dpt,
+                    np.zeros_like(norm_dpt, dtype=int), dpt)
 
-        # Project dpt on line (v1, v0)
+        # Project dpt on line (v0, v1)
         dproj = dproj / norm_dproj
         proj_lens = np.dot(dpt, dproj.T)
         tangent_vecs = proj_lens * dproj
 
-        # If projected length is not in [0, 1] it is outside the line element
-        # and the distance to v0 (<0) or v1 (>len(pt)) is returned
-        m0 = np.ravel(proj_lens < 0.)
-        m1 = np.ravel(proj_lens > norm_dproj)
+        # If projected length is not in [0, norm_dproj] it is outside the line
+        # element and the distance to v0 (<0) or v1 (>norm_dproj) is returned
+        m0 = np.squeeze(proj_lens < 0., axis=1)  # Make mask 1D again -> axis 0
+        m1 = np.squeeze(proj_lens > norm_dproj, axis=1)
         # The projection vectors are orthogonal to the line or point to v0, v1
         proj_vecs = tangent_vecs - dpt  # projected on segment line
-        proj_vecs[m0] = -dpt[m0]  # proj_len < 0
-        proj_vecs[m1] = v1 - pt[m1]  # proj_len > norm_dproj
+        proj_vecs[m0] = -dpt[m0]  # proj_len < 0 -> project to v0
+        proj_vecs[m1] = v1 - pts[m1]  # proj_len > norm_dproj -> project to v1
 
         # Encode if the distance is closest to vertex 0 (id=0), the segment
         # (id=-1) or vertex 1 (id=1)
@@ -343,7 +342,34 @@ class Polyline(object):
         proj_to[m0] = 0
         proj_to[m1] = 1
 
-        return proj_vecs, np.linalg.norm(proj_vecs, axis=1), proj_to
+        # Get distances
+        dists = np.linalg.norm(proj_vecs, axis=1)
+
+        # Get distance gradients [dL/dx, dL/dy], is only valid for each
+        # projection region, not continuous at the borders.
+        # Gradient for points with distance 0 (on the segment) are set to zero.
+        # Valid gradients are normed negative distance vectors (pt in direction
+        # of greatest change, which is away from the projecting element)
+        dists_grad = np.zeros((len(dists), 2), dtype=np.float)
+        valid = dists > 0
+        if np.any(valid):
+            dists_grad[valid] = (
+                -proj_vecs[valid] / dists.reshape(len(proj_vecs), 1)[valid])
+
+        return dists, dists_grad, proj_to, proj_vecs
+
+    def _check_idx(self, idx):
+        """ Checks if idx is a single integer, otherwise logic breaks """
+        err = "idx must be an integer."
+        try:
+            idx + 1
+        except Exception:
+            raise ValueError(err)
+        if isinstance(idx, np.ndarray):
+            raise ValueError(err)
+
+    def _check_vertices(self, v):
+        return
 
     @property
     def nvertices(self):
